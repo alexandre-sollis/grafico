f3f2da43f50752fa7d059efc451e31963c0d434b
Alexandre Oliveira da Silva <alexandre.olives@gmail.com>
2021-09-11 18:11:33 -0400

    update

--- parabola/parabola.py
+++ parabola/parabola.py
@@ -3,19 +3,22 @@ from matplotlib import pyplot as plt
 from matplotlib import animation
 
 fig = plt.figure()
-ax = plt.axes(xlim=(0,3), ylim=(-2,2))
-line, = ax.plot([], [], lw=2)
+fig.patch.set_facecolor('black')
+ax = plt.axes(xlim = (-1,4), ylim = (-1,3))
+ax.patch.set_facecolor('black')
+line, = ax.plot([], [], lw = 2)
+
 
 def init():
     line.set_data([],[])
     return line,
 
 def animate(i):
-    x = np.linspace(0,3,500)
+    x = np.linspace(-3,6,500)
     y = 0.01*i*(x-1)*(x-2)
     line.set_data(x,y)
     return line,
 
-anim = animation.FuncAnimation(fig, animate, init_func=init, frames=200, interval=20, blit=True)
-
+anim = animation.FuncAnimation(fig, animate, init_func = init, 
+frames = 200, interval = 20, blit = True)
 plt.show()
